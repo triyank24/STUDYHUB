273b0f3590d7a9b568bd1dda9eb004a4cde3f8e3
Triyank Agarwal <rajtriyank25@gmail.com>
2026-05-25 15:38:23 +0530

    fix gmail smtp config

--- config/mail.js
+++ config/mail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
